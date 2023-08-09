379de34da020af4abfb05e44e538c5d8ee8e8aae
aritradey-CS <aritra1999.dey@gmail.com>
2023-08-09 21:55:50 +0530

    If this test case wins we will add this components to the App.js

--- src/ApiTestPage.js
+++ src/ApiTestPage.js
@@ -5,7 +5,7 @@ const API_KEY = "e151eede51858a0862be634a32f83d9c";
 const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
 
 const ApiTestPage = () => {
-  const [movieRecommendation, setMovieRecommendation] = useState("");
+  const [movieRecommendations, setMovieRecommendations] = useState([]); // Change this line
 
   const fetchMovieRecommendation = async () => {
     try {
@@ -15,18 +15,36 @@ const ApiTestPage = () => {
       const moviePrice = "$15.99"; // Replace with real price from another API
       const movieReview = "Great movie!";
       const recommendation = `I recommend the movie '${movieTitle}' with a rating of ${movieRating}/10. It costs ${moviePrice}. Review: ${movieReview}`;
-      setMovieRecommendation(recommendation);
+      
+      // Add new recommendation to the existing list
+      setMovieRecommendations((prevRecommendations) => [
+        ...prevRecommendations,
+        recommendation,
+      ]);
     } catch (error) {
       console.error("Error fetching movie data:", error);
-      setMovieRecommendation("Oops! Something went wrong while fetching movie data.");
+      setMovieRecommendations([
+        "Oops! Something went wrong while fetching movie data.",
+      ]);
     }
   };
 
   return (
     <div>
       <h2>Movie API Test</h2>
-      <button onClick={fetchMovieRecommendation}>Fetch Movie Recommendation</button>
-      {movieRecommendation && <p>{movieRecommendation}</p>}
+      <button onClick={fetchMovieRecommendation}>
+        Fetch Movie Recommendations
+      </button>
+      {movieRecommendations.length > 0 && (
+        <div>
+          <h3>Movie Recommendations:</h3>
+          <ul>
+            {movieRecommendations.map((recommendation, index) => (
+              <li key={index}>{recommendation}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
