5198aa85031bc76635aa32797f27434d64069ecb
aritradey-CS <aritra1999.dey@gmail.com>
2023-08-10 22:15:30 +0530

    clothes,books & music searching availabel

---
+++ src/components/BookRecommendation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BookRecommendation = ({ recommendations }) => {
+  return (
+    <div className="recommendation-section">
+      <h3>Book Recommendations:</h3>
+      {/* Render book recommendations here */}
+    </div>
+  );
+};
+
+export default BookRecommendation;
